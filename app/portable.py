37a5aeb21eaa19271d944d6536158a99b4729a1f
atgithub2 <101512318+atgithub2@users.noreply.github.com>
2026-04-06 15:31:59 +0400

    Update Googlebot headers for user agent and accept

--- app/portable.py
+++ app/portable.py
@@ -6,7 +6,9 @@ from urllib.parse import urlparse, urljoin
 
 app = flask.Flask(__name__)
 googlebot_headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.119 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
 }
 html = """
 <!DOCTYPE html>
